c003b40b2603e8fcc55c74c2d5f8d12c1303158b
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-14 23:02:43 +0530

    Email List Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import { Header } from "../../components/header/Header";
 import { Featured } from "../../components/featured/Featured";
 import { PropertyList } from "../../components/propertyList/PropertyList";
 import { FeaturedProperties } from "../../components/featuredProperties/FeaturedProperties";
+import { MailList } from "../../components/mailList/MailList";
 
 export const Home = () => {
   return (
@@ -17,6 +18,7 @@ export const Home = () => {
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
         <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   );
